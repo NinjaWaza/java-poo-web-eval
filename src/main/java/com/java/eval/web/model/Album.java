d26f1b5b47b5521c7b9798197734808de1173d15
Théodore <theodore.follet@gmail.com>
2020-11-12 15:22:34 +0100

    Album class and Artist class initialized

--- src/main/java/com/java/eval/web/model/Album.java
+++ src/main/java/com/java/eval/web/model/Album.java
@@ -1,4 +1,48 @@
 package com.java.eval.web.model;
 
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="album")
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column(name = "AlbumId") //Maybe useful because the id isn't id in database but AlbumId
+    private Integer albumId;
+
+    @Column(name = "Title")
+    private String title;
+
+    @Column(name = "ArtisteId")
+    private Integer artistId;
+
+    //Getters
+
+    public Integer getAlbumId(){
+        return this.albumId;
+    }
+
+    public String getTitle(){
+        return this.title;
+    }
+
+    public Integer getArtistId(){
+        return this.artistId;
+    }
+
+    //Setters
+
+    public void setAlbumId(Integer albumId){
+        this.albumId = albumId;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setArtistId(Integer artistId){
+        this.artistId = artistId;
+    }
 }
--- src/main/java/com/java/eval/web/model/Artist.java
+++ src/main/java/com/java/eval/web/model/Artist.java
@@ -1,4 +1,36 @@
 package com.java.eval.web.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="artist")
 public class Artist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    //@Column(name = "ArtistId") //Maybe useful because the id isn't id in database but ArtistId
+    private Integer artistId;
+
+    @Column(name = "Name")
+    private String name;
+
+    //Getters
+
+    public Integer getArtistId(){
+        return this.artistId;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    //Setters
+
+    public void setArtistId(Integer artisteId){
+        this.artistId = artisteId;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
 }
